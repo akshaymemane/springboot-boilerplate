53a20501999f2a58b44323ea970b1f806e0bbe3e
RupaliGuleria <guleriarupali27@gmail.com>
2024-02-29 11:17:16 +0530

    basic setup

---
+++ src/main/java/com/onewill/MicroserviceBoilerplateProjectApplication.java
@@ -0,0 +1,13 @@
+package com.onewill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceBoilerplateProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceBoilerplateProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onewill/MicroserviceBoilerplateProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onewill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceBoilerplateProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
